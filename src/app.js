033f8a874b9ad2d8650668c912f62be928dd9457
Aditya Sharma <aditya.sharma4231.in@gmail.com>
2025-06-30 13:23:32 +0530

    Understanding the routing properlly

--- src/app.js
+++ src/app.js
@@ -2,12 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use("/test",(req, res)=>{
-    res.send("Hello from the Test side! This is the Test route. ");
+// app.use("/test",(req, res)=>{
+//     res.send("Hello from the Test side! This is the Test route. ");
+// })
+// app.use("/home",(req, res)=>{
+//     res.send("Hello from the Home side! This is the Home route.");
+// })
+
+// this only call when the user use get method with user route ( to get the information of the user)
+app.get("/user", (req,res)=>{
+    res.send({firstName:"Aditya", lastName:"Sharma"})
 })
-app.use("/home",(req, res)=>{
-    res.send("Hello from the Home side! This is the Home route.");
+
+// this is only call when the user post method with user route ( to post some details to the DB)
+app.post("/user", (req,res)=>{
+    // Logic for the Saving dataBase
+    res.send("The data is succefully save in the database!!")
 })
+
 app.use((req, res)=>{
     res.send("Hello from the Server side! This is the default response.");
 })
